879ab549143bcc3f6ce8be7117631d39a4c3a22d
Acan <xie.zeyu20@gmail.com>
2024-03-17 23:37:13 +0800

    .

--- assignments/hw-1/main.cpp
+++ assignments/hw-1/main.cpp
@@ -14,26 +14,23 @@ Bint e, d;
 
 int main()
 {
-    // p = randomBigPrime(8);
-    // q = randomBigPrime(8);
+    p = randomBigPrime(2);
+    q = randomBigPrime(2);
 
-    // n = p * q;
-    // phi_n = (p - Bint(1)) * (q - Bint(1));
+    n = p * q;
+    phi_n = (p - Bint(1)) * (q - Bint(1));
 
-    // cout<<"p: "<<p<<"\n";
-    // cout<<"q: "<<q<<"\n";
-    // cout<<"n: "<<n<<"\n";
-    // cout<<"phi_n: "<<phi_n<<"\n";
+    e = Bint(65537);
 
-    // e = Bint(65537);
+    Bint tmp;
+    Bint gcd = extendedEuclidean(e, phi_n, d, tmp);
 
-    Bint a(35), b(47);
-
-    Bint x, y;
-
-    cout << extendedEuclidean(a, b, x, y) << "\n";
-
-    cout << x << " " << y << "\n";
+    cout << "p: " << p << "\n";
+    cout << "q: " << q << "\n";
+    cout << "n: " << n << "\n";
+    cout << "phi_n: " << phi_n << "\n";
+    cout << "e: " << e << "\n";
+    cout << "d: " << d << "\n";
 
     return 0;
 }
